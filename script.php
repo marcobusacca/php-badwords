94b1baadbf729a3c3db34226d93b9031cb6b0f83
marcobusacca <marcobusacca01@gmail.com>
2023-06-30 15:31:10 +0200

    Esercizio Completato

--- script.php
+++ script.php
@@ -7,5 +7,11 @@ $censoredWord = $_GET['censored-word'];
 $paragraph = $_GET['paragraph'];
 
 // STAMPO IL CONTENUTO DI PARAGRAPH E LA LUNGHEZZA DEL CONTENUTO
-echo $paragraph." ".strlen($paragraph);
+echo "PARAGRAFO E LUNGHEZZA"."<br>".$paragraph." -----> ".strlen($paragraph)."<br><br>";
+
+// CENSURO LA PAROLA SCELTA DALL'UTENTE IN PRAGRAPH E INSERISCO IL RISULTATO IN CENSORED_PARAGRAPH
+$censoredParagraph = str_replace($censoredWord, '***', $paragraph);
+
+// STAMPO IL CONTENUTO DI CENSORED_PARAGRAPH E LA LUNGHEZZA DEL CONTENUTO
+echo "PARAGRAFO CENSURATO E LUNGHEZZA"."<br>".$censoredParagraph." -----> ".strlen($censoredParagraph);
 ?>
